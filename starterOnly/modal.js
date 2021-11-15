871de8c8381406df5908248cdb5fa0f291ebe20c
LudovicSinquet <sinquet@neuf.fr>
2021-11-15 14:13:32 -0400

    fermeture X modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,22 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// DOM ELEMENTS MODAL 
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName('close');
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
+// ------ DISPLAY MODAL ------ //
+// LAUNCH MODAL EVENTS
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
+// LAUNCH MODAL FORM
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block';
 }
-
+// CLOSE MODAL FORM
+function closeModal() {
+  modalbg.style.display = 'none';
+}
+closeBtn[0].addEventListener('click', closeModal);
 
