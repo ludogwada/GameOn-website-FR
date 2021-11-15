2efb8aab1ca6fbc609028923828df9f9d059ebb4
LudovicSinquet <sinquet@neuf.fr>
2021-11-15 15:29:22 -0400

    check email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName('close');
+const email = document.getElementById("email");
 
 // ------ DISPLAY MODAL ------ //
 // LAUNCH MODAL EVENTS
@@ -26,3 +27,32 @@ function closeModal() {
 }
 closeBtn[0].addEventListener('click', closeModal);
 
+const form = document.getElementsByName("formModal");
+let emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+//FIRST NAME
+//const firstName = document.getElementById("first");
+
+//function checkFirstName(){
+//  if firstName
+//}
+
+//EMAIL
+email.addEventListener("change",function(){
+  checkEmail(this);
+})
+const checkEmail = function(inputEmail){
+  let emailRegExp = new RegExp(
+    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+  let testEmail = emailRegExp.test(inputEmail.value);
+  
+  if (testEmail){
+    email.parentElement.setAttribute('data-error-visible', 'false');
+    email.style.border = 'solid #279e7a 0.19rem';
+    return true;
+    }
+    email.parentElement.setAttribute('data-error-visible', 'true');
+    email.style.border = '2px solid #e54858';
+    return false;
+}
