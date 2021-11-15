5655b78384bcbc9131d41fd160192f83abfa7c0d
LudovicSinquet <sinquet@neuf.fr>
2021-11-15 16:45:16 -0400

    check name

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,18 +1,22 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
 // DOM ELEMENTS MODAL 
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const formData = document.querySelectorAll('.formData');
 const closeBtn = document.getElementsByClassName('close');
-const email = document.getElementById("email");
+const form = document.getElementsByName('formModal');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
 
 // ------ DISPLAY MODAL ------ //
 // LAUNCH MODAL EVENTS
@@ -27,32 +31,45 @@ function closeModal() {
 }
 closeBtn[0].addEventListener('click', closeModal);
 
-const form = document.getElementsByName("formModal");
-let emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 //FIRST NAME
-//const firstName = document.getElementById("first");
+firstName.addEventListener('change', function(){
+  checkFirst(this);
+})
+ const checkFirst = function inputFirstName(){
+  if (firstName.value.trim().length < 2 || first.value.trim() === '' || !firstName.value.match(regex)) {
+    first.parentElement.setAttribute('data-error-visible', 'true');
+    return false;
+  }
+    first.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+}
 
-//function checkFirstName(){
-//  if firstName
-//}
+//LASTNAME
+lastName.addEventListener('change', function(){
+  checkLast(this);
+})
+const checkLast = function inputLastName(){
+  if (lastName.value.trim().lenght < 2 || lastName.value.trim() === '' || !lastName.value.match(regex)) {
+    last.parentElement.setAttribute('data-error-visible', 'true');
+    return false;
+  }
+    last.parentElement.setAttribute('data-error-visible', 'false');
+    return true;
+}
+
 
 //EMAIL
-email.addEventListener("change",function(){
+email.addEventListener('change',function(){
   checkEmail(this);
 })
-const checkEmail = function(inputEmail){
-  let emailRegExp = new RegExp(
-    '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
-
-  let testEmail = emailRegExp.test(inputEmail.value);
+const checkEmail = function inputEmail (){   
+  const RegExp = '^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$';
   
-  if (testEmail){
+  if (email.value.trim().match(RegExp)){
     email.parentElement.setAttribute('data-error-visible', 'false');
-    email.style.border = 'solid #279e7a 0.19rem';
     return true;
     }
     email.parentElement.setAttribute('data-error-visible', 'true');
-    email.style.border = '2px solid #e54858';
     return false;
 }
