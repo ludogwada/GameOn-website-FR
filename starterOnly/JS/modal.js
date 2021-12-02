0de1ce41e9617a7f8c4b777d7a33d70d60e7f477
ludogwada <sinquet@neuf.fr>
2021-12-02 10:08:25 -0400

    add gitignore editorconfig correction bug js

--- starterOnly/JS/modal.js
+++ starterOnly/JS/modal.js
@@ -47,116 +47,40 @@ function closeModalTh() {
     document.location.reload();//document rechargé
 }
 
-
 //FIRST NAME
 firstName.addEventListener('focusout', function(){
   checkFirst(this);
-})
-function checkFirst() {
-  //si valeur de firstName < 2 ou vide ou !du regex attribue true à "data-error-visible" et retourne faux
-  if (firstName.value.trim().length < 2 || firstName.value.trim() === '' || !firstName.value.match(regex)) {
-    firstName.parentElement.setAttribute('data-error-visible', 'true');
-    return false;
-    //sinon attribue false à "data-error-visible" et retourne vrai
-  } else (
-  firstName.parentElement.setAttribute('data-error-visible', 'false'));
-  return true;
-}
+});
 
 //LASTNAME
 lastName.addEventListener('focusout', function(){
   checkLast(this);
-})
-function checkLast() {
-  //si valeur de lastName < 2 ou vide ou !du regex attribue true à "data-error-visible" et retourne faux
-
-  if (lastName.value.trim().length < 2 || last.value.trim() === '' || !lastName.value.match(regex)) {
-    lastName.parentElement.setAttribute('data-error-visible', 'true');
-    return false;
-    //sinon attribue false à "data-error-visible" et retourne vrai
-  } else (
-  lastName.parentElement.setAttribute('data-error-visible', 'false'));
-  return true;
-}
+});
 
 //EMAIL
 email.addEventListener('focusout', function(){
   checkEmail(this);
-})
-function checkEmail() {
-    //si valeur de email match à la constante RegExp attribue false à "data-error-visible" et retourne vrai
-  if (email.value.trim().match(RegExp)) {
-    email.parentElement.setAttribute('data-error-visible', 'false');
-    return true;
-    //sinon attribue true à "data-error-visible" et retourne faux
-  } else (
-  email.parentElement.setAttribute('data-error-visible', 'true'));
-  return false;
-}
+});
 
 //BIRTHDATE
 birthdate.addEventListener('focusout', function(){
   checkBirthdate(this);
-})
-function checkBirthdate() {
-  // valeur de birthdate !== 10 et isNAN ===true attribue true à "data-error-visible" et retourne faux
-  if (birthdate.value.trim().length !== 10 || isNaN(quantity.value.trim()) === true) {
-    birthdate.parentElement.setAttribute('data-error-visible', 'true');
-    return false;
-    //sinon attribue false à "data-error-visible" et retourne vrai
-} else(
-birthdate.parentElement.setAttribute('data-error-visible', 'false'));
-return true;
-}
+});
 
 //TOURNAMENTS
 quantity.addEventListener('focusout', function(){
-  checkQuantity(this);
-})
-function checkQuantity() {
-  //si valeur de quantity === 0 , isNaN ===true et < 0 attribue true à "data-error-visible" et retourne faux
-  if (quantity.value.trim().length === 0 || isNaN(quantity.value.trim()) === true || quantity.value.trim() < 0) {
-    quantity.parentElement.setAttribute('data-error-visible', 'true');
-    return false;
-    //sinon attribue false à "data-error-visible" et retourne vrai
-  } else (
-  quantity.parentElement.setAttribute('data-error-visible', 'false'));
-  return true;
-}
+    (this);
+});
 
 //LOCATIONS
 allLocations.addEventListener('change', function() {
   checkLocations(this);
-})
-function checkLocations() {
-  // pour (varible i = 0 ; i < location.length ; i= i+1 
-    for( let i = 0 ; i < locations.length; i++)
-    //si i est coché attribue false à "data-error-visible" et retourne vrai
-      if (locations[i].checked) {
-          allLocations.setAttribute('data-error-visible', 'false');
-          return true;
-          //sinon attribue true à "data-error-visible" et retourne faux
-      } else {
-        allLocations.setAttribute('data-error-visible', 'true');
-      }
-    return false;
-}
+});
 
 //TERMS OF USE
 termOfUse.addEventListener('change', function() {
   checkTermOfUse(this);
-})
-function checkTermOfUse() {
-  // si termOfUse validé attribue false à "data-error-visible" et retourne vrai
-    if (termOfUse.checked) {
-      termOfUse.parentElement.setAttribute('data-error-visible', 'false');
-      return true
-    } else{
-          //sinon attribue true à "data-error-visible" et retourne faux
-      termOfUse.parentElement.setAttribute('data-error-visible', 'true');
-    return false;
-  }
-}
+});
 
 //SUBMIT FORM
 form.addEventListener('submit', function(e) {
@@ -177,7 +101,8 @@ form.addEventListener('submit', function(e) {
       checkBirthdate(birthdate) &&
       checkQuantity(quantity) &&
       checkLocations(allLocations) &&
-      checkTermOfUse(termOfUse)) {
+      checkTermOfUse(termOfUse)
+      ) {
         form.style.display ='none';
         modalTh.style.display ='block';
         form.reset();
@@ -186,4 +111,3 @@ form.addEventListener('submit', function(e) {
           modalTh.style.display ='none';
       }
 });
-
